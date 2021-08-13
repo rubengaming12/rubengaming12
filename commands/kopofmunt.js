338443b2f382d2a2f4133358b48fd3d06e40fd57
rubengaming1 <79492648+rubengaming1@users.noreply.github.com>
2021-08-14 00:02:04 +0200

    kopofmunt
    minigame

---
+++ commands/kopofmunt.js
@@ -0,0 +1,16 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    var value = ["kop", "munt"];
+
+    var result = value[Math.floor(Math.random() * value.length)];
+
+    message.channel.send(`Ik had **${result}** in gedachten`);
+
+
+}
+
+module.exports.help = {
+    name: "kopofmunt"
+}
