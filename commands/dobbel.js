f4b282f01c8b677b21bb3c8ddf97aad929d35527
rubengaming1 <79492648+rubengaming1@users.noreply.github.com>
2021-08-13 23:58:44 +0200

    dobbel

---
+++ commands/dobbel.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    var result = Math.ceil(Math.random() * 6);
+
+    message.channel.send(`:game_die: Je hebt **${result}** gegooid! :game_die:`);
+
+
+}
+
+module.exports.help = {
+    name: "dobbel"
+}
