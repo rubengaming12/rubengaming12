562b6fc812773a1937211efda0c8023e32e9f91b
rubengaming1 <79492648+rubengaming1@users.noreply.github.com>
2021-08-14 16:06:15 +0200

    avatar command + botconfig aanpassing

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,19 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    
+    var member = message.guild.member(message.mentions.users.first() || client.users.cache.get(args[0]) || 
+        client.users.cache.find(user => user.username.toLowerCase() == args.join(" ").toLowerCase()));
+        client.users.cache.find(user => user.username.toLowerCase() == args.join(" ").toLowerCase() ||
+        client.users.cache.find(user => user.tag.toLowerCase() == args.join(" ").toLowerCase()));
+
+    if(!member) member = message.member;
+
+    var embed = new discord.MessageEmbed()
+        .setTitle(`Avatar ${member.user.username}`)
+        .setColor("BLUE")
+        .setImage(member.user.displayAvatarURL({ dynamic, size: 4096 }));
+
+    message.channel.send(embed);
 
 }
 
