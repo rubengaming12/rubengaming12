c9c603a47aa074ba20c00ea04751afc4bb5e9b1e
rubengaming1 <79492648+rubengaming1@users.noreply.github.com>
2021-08-21 23:11:59 +0200

    sps

--- commands/bladsteenschaar.js
+++ commands/bladsteenschaar.js
@@ -4,10 +4,9 @@ module.exports.run = async (client, message, args) => {
 
     // sps steen, papier, schaar
 
+    if (!args[0]) return message.reply("Gebruik sps <steen, paapier, schaar>");
 
-    if (!args[0]) 
-
-    var options = ["steen", "papier", "schaar"]; return message.reply("Gebruik sps <steen, papier, schaar>")
+    var options = ["steen", "papier", "schaar"];
 
     var result = options[Math.floor(Math.random() * options.length)];
 
