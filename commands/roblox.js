6d1b49154be585004d6f0c76e3edae9b6f37aadf
rubengaming1 <79492648+rubengaming1@users.noreply.github.com>
2021-08-22 19:35:19 +0200

    roblox naam

---
+++ commands/roblox.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    message.channel.send("Ruben zijn roblox naam is:\n**kiansadler6556**\n\nhttps://www.roblox.com/users/471176128/profile")
+
+}
+
+module.exports.help = {
+    name: "roblox",
+    description: "",
+    category: ""
+}
