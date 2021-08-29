ec85ca77a76dd8dafd664e9676997ef8ab02582c
rubengaming1 <79492648+rubengaming1@users.noreply.github.com>
2021-08-29 16:25:58 +0200

    haarlem command

---
+++ commands/haarlem.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    return message.channel.send("https://discord.gg/R8Ctk7sq5t");
+
+}
+
+module.exports.help = {
+    name: "haarlem",
+}
