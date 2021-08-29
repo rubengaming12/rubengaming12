0fe9b5906ec73cf86a2e999699d6eddca8651506
rubengaming1 <79492648+rubengaming1@users.noreply.github.com>
2021-08-30 01:24:36 +0200

    nieuwe index

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-const discord = require("discord.js");
-const botConfig = require("./botconfig.json");
-const database = require("./database.json");
-const mysql = require("mysql");
-const levelFile = require("./data/levels.json");
+const Discord = require("discord.js");
+const botConfig = require("./botconfig.json")
+const fileLevels = require("./data/levels.json")
 
-const activeSongs = new Map();
-
-//  Command handler
 const fs = require("fs");
-const { isFunction } = require("util");
 
-const client = new discord.Client();
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-
-//  Command handler
-client.commands = new discord.Collection();
-
-
-client.login(process.env.token);
-
-var con = mysql.createConnection({
-    host: database.host,
-    user: database.user,
-    password: database.password,
-    database: database.database
-});
-
-
-
-//  Command handler
 fs.readdir("./commands/", (err, files) => {
 
     if (err) console.log(err);
@@ -36,137 +14,35 @@ fs.readdir("./commands/", (err, files) => {
     var jsFiles = files.filter(f => f.split(".").pop() === "js");
 
     if (jsFiles.length <= 0) {
-        console.log("Kon geen files vinden");
+        console.log("Can't find any files");
         return;
     }
 
     jsFiles.forEach((f, i) => {
 
         var fileGet = require(`./commands/${f}`);
-        console.log(`De file ${f} is geladen`);
+        console.log(`The File ${f} Got Loaded`);
 
-        client.commands.set(fileGet.help.name, fileGet);
-    });
+        // client.commands.set(fileGet.help.name, fileGet);
+        if (fileGet.help && fileGet.help.name) {
+            client.commands.set(fileGet.help.name, fileGet);
+        } else {
+            console.error(`file ${f} does not have .help or .help.name property!`);
 
-});
+        };
 
-
-client.on("guildMemberAdd", member => {
-
-    // var role = member.guild.roles.cache.get('462166173690232842');
-
-    // if (!role) return;
-
-    // member.roles.add(role);
-
-
-    con.query(`SELECT IDRole FROM rollen WHERE IDUser = '${member.user.id}'`, (err, rows) => {
-
-        if (err) throw err;
-
-        if (rows.length > 0) {
-
-            for (let index = 0; index < rows.length; index++) {
-                const role = rows[index];
-
-                member.roles.add(role.IDRole);
-            }
-
-        }
-
-    });
-
-
-    var channel = member.guild.channels.cache.get('878450076031262730');
-
-    if (!channel) return;
-
-    // channel.send(`Welkom bij de server ${member}`);
-
-    var joinEmbed = new discord.MessageEmbed()
-        .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
-        .setDescription(`Hoi ${member.user.username}, **Welkom op de server**`)
-        .setColor("GREEN")
-        .setFooter("Gebruiker gejoined")
-        .setTimestamp();
-
-    channel.send(joinEmbed);
+    })
 
 });
 
 
-client.on("guildMemberRemove", member => {
-
-    var channel = member.guild.channels.cache.get('878450090367402024');
-
-    if (!channel) return;
-
-    var leaveEmbed = new discord.MessageEmbed()
-        .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
-        .setColor("RED")
-        .setFooter("Gebruiker geleaved")
-        .setTimestamp();
-
-    channel.send(leaveEmbed);
-
-});
-
-client.on('message', async message => {
-	if (message.author.bot) return;
-		  if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
-		    const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
-		    await db.delete(`afk-${message.author.id}+${message.guild.id}`)
-		    message.reply(`Je AFK is verwijderd!`)
-		  }
-	if(message.mention.members.first()) {
-	  if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
-	    message.channel.send(db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`))
-	  }else return;
-	}else;
-
-	var prefix = process.env['prefix']
-
-	var messageArray = message.content.split(' ');
-
-	var command = messageArray[0];
-
-	if (!message.content.startsWith(prefix)) return;
-
-	var arguments = messageArray.slice(1);
-
-	var commands = client.commands.get(command.slice(prefix.length));
-
-	if (commands) commands.run(client, message, arguments);
-});
-
-
+client.login(botConfig.token);
 
 client.on("ready", async () => {
 
     console.log(`${client.user.username} is online.`);
 
-    client.user.setActivity("Kijkt naar Ruben Gaming ", { type: "PLAYING" });
-
-});
-
-
-client.on("messageDelete", messageDeleted => {
-
-    if (messageDeleted.author.bot) return;
-
-    var content = messageDeleted.content;
-    if (!content) content = "Geen tekst te vinden";
-
-    var respone = `Bericht ${messageDeleted.id} is verwijderd uit ${messageDeleted.channel}\n **Bericht:** ${content}`;
-
-    var embed = new discord.MessageEmbed()
-        .setTitle("Verwijderd bericht")
-        .setAuthor(`${messageDeleted.author.id} ${messageDeleted.author.tag}`, `${messageDeleted.author.avatarURL({ size: 4096 })}`)
-        .setDescription(respone)
-        .setTimestamp()
-        .setColor("#FF0000");
-
-    client.channels.cache.find(c => c.name == "berichten-logs").send(embed);
+    client.user.setActivity("Watch Eyes", { type: "Playing" });
 
 });
 
@@ -174,114 +50,136 @@ client.on("messageUpdate", async (oldMessage, newMessage) => {
 
     if (newMessage.author.bot) return;
 
-    var embed = new discord.MessageEmbed()
-        .setTitle("Aangepast bericht")
-        .setAuthor(`${newMessage.author.tag} (${newMessage.author.id})`, newMessage.author.avatarURL({ size: 4096 }))
-        .setDescription(`Bericht ${newMessage} is bewerkt in ${newMessage.channel}\n **Voor:** ${oldMessage.content}\n**Na:** ${newMessage.content}`)
+    if(oldMessage.content == newMessage.content) return;
+
+    var embed = new Discord.MessageEmbed()
+        .setAuthor((`${newMessage.author.tag}`),newMessage.author.avatarURL({ size: 4096 }))
+        .setDescription(`**Message:** ${newMessage} **got edited in** ${newMessage.channel}\n **Before:** ${oldMessage}`)
         .setTimestamp()
-        .setColor("BLUE");
+        .setColor('#00f00');
 
-    client.channels.cache.get('875777490554085417').send(embed);
-
+    client.channels.cache.get('854299795551944744').send(embed);
 
 });
-
-// anti link
-
-// client.on('message', (message) => {
-//     if (!message.member.roles.cache.get('840528715065589801')) return
-//     if (!message.member.roles.cache.get('840530451877068800')) return
-//     if (!message.member.roles.cache.get('842833429728460840')) return
-//     message.member.guild.channels.cache.get("840551757892681759");
-//     if (message.content.includes('discord.gg/'||'discordapp.com/invite/')) {
-//       message.delete()
-//         message.channel.send(`${message.author} Uw links zijn verwijderd uit dit kanaal`);
-//     }
-
-// })
-
-// var swearWords = ["koe", "kalf", "varken"]; 
 
 client.on("message", async message => {
 
     if (message.author.bot) return;
-// wat mis je ookalweer?
-    if (message.channel.type === "dm") return; 
 
-
-    // var msg = message.content.toLowerCase();
-
-    // for (let i = 0; i < swearWords["vloekwoorden"].length; i++) {
-
-    //     if (msg.includes(swearWords["vloekwoorden"][i])) {
-
-    //         message.delete();
-
-    //         return message.reply("Gelieve niet te vloeken").then(msg => msg.delete({ timeout: 3000 }));
-
-    //     }
-
-    // }
-
+    if (message.channel.type == "dm") return;
 
     var prefix = botConfig.prefix;
 
     var messageArray = message.content.split(" ");
 
-
-    var swearWords = JSON.parse(fs.readFileSync("./data/swearWords.json"));
-
-    var senteceUser = "";
-    var amountSwearWords = 0;
-
-    for (let y = 0; y < messageArray.length; y++) {
-
-        const word = messageArray[y].toLowerCase();
-
-        var changeWord = "";
-
-        for (let i = 0; i < swearWords["vloekwoorden"].length; i++) {
-
-            if (word.includes(swearWords["vloekwoorden"][i])) {
-
-                changeWord = word.replace(swearWords["vloekwoorden"][i], "******");
-
-                senteceUser += " " + changeWord;
-
-                amountSwearWords++;
-
-            }
-
-        }
-
-        if (!changeWord) {
-            senteceUser += " " + messageArray[y];
-        }
-
-    }
-
-    if (amountSwearWords != 0) {
-
-        message.delete();
-        message.channel.send(senteceUser);
-        message.channel.send("Niet vloeken a.u.b.");
-    }
-
-
-
     var command = messageArray[0];
-
-    if (!message.content.startsWith(prefix)) return;
-
-    //  Command handler
-    var arguments = messageArray.slice(1);
 
     var commands = client.commands.get(command.slice(prefix.length));
 
-    var options = {
-        active: activeSongs
-    };
+    var args = message.content.slice(prefix.lengh).split(/ +/);
 
-    if (commands) commands.run(client, message, arguments, options);
+    if (commands && message.content.startsWith(prefix)) commands.run(client, message, args);
 
-});
+    var randomXp = Math.floor(Math.random(1) * 25) + 1;
+
+    var idUser = message.author.id;
+
+    if (!fileLevels[idUser]) {
+
+        fileLevels[idUser] = {
+
+            xp: 0,
+            level: 0
+
+        };
+
+    }
+
+    fileLevels[idUser].xp += randomXp;
+
+    var levelUser = fileLevels[idUser].level;
+    var xpUser = fileLevels[idUser].xp;
+    var nextLevelXp = levelUser * 500;
+
+    if (nextLevelXp === 0) nextLevelXp = 100;
+
+    console.log(nextLevelXp + " " + xpUser);
+
+    if (xpUser > nextLevelXp) {
+
+        fileLevels[idUser].level += 1;
+
+        fileLevels[idUser].xp = 0;
+
+        var embedLevel = new Discord.MessageEmbed()
+            .setTitle((`${message.author.tag}`),message.author.avatarURL({ size: 4096 }))
+            .setDescription("***Level Up***")
+            .setColor("#29e53f")
+            .addField("New Level: ", fileLevels[idUser].level);
+
+        message.channel.send(embedLevel);
+
+        var channel = message.member.guild.channels.cache.get("865019819497226250");
+
+    }
+
+    fs.writeFile("./data/levels.json", JSON.stringify(fileLevels), err => {
+
+        if (err) console.log(err);
+
+    });
+
+    if(fileLevels[idUser].level == 5){
+
+        var role = message.guild.roles.cache.find(r => r.name == "OG");
+
+        var member = message.member;
+        member.roles.add(role);
+
+        var embedLevel = new Discord.MessageEmbed()
+            .setTitle((`${message.author.tag}`),message.author.avatarURL({ size: 4096 }))
+            .setDescription((`${message.author.tag}`),"***Unlocked the role Found (LVL 5+)***")
+            .setColor("#29e53f")
+            .addField("New Level: ", fileLevels[idUser].level);
+        message.channel.send(embedLevel);
+
+    } else {
+        var embedLevel = new Discord.MessageEmbed()
+            .setTitle((`${message.author.tag}`),message.author.avatarURL({ size: 4096 }))
+            .setDescription("***Level Up***")
+            .setColor("#29e53f")
+            .addField("New Level: ", fileLevels[idUser].level);
+        message.channel.send(embedLevel);
+    
+    }
+
+
+    if (command === `${prefix}hallo`) {
+
+        return message.channel.send("hallo!");
+
+    }
+
+
+})
+
+
+
+
+
+//});
+
+
+async function promptMessage(message, author, time, reactions) {
+
+    time *= 1000;
+
+    for (const reaction of reactions) {
+        await message.react(reaction);
+    }
+
+    var filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === author.id;
+
+    return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
+
+}
